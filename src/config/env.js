b79bd32835d7c122a80003b9835a8c92eb284599
maikon alexandre <maikonalexandre574@gmail.com>
2026-03-13 14:03:31 -0300

    feat: add another route integration

--- src/config/env.js
+++ src/config/env.js
@@ -1,16 +1,17 @@
-const z = require("zod")
-require('dotenv').config();
+const z = require("zod");
+require("dotenv").config();
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
-    BACKEND_INTEGRACAO_ANOTAAI_PDV7_URL: z.string().url()
-})
+  NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
+  BACKEND_INTEGRACAO_ANOTAAI_PDV7_URL: z.string().url().optional(),
+  BACKEND_INTEGRACAO_KEETA_PDV7_URL: z.string().url().optional(),
+});
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-	console.error('🟥 [ERRO]: Variavies ambiente', _env.error.format());
-	throw new Error('Variavies ambiente não encontradas');
+  console.error("🟥 [ERRO]: Variavies ambiente", _env.error.format());
+  throw new Error("Variavies ambiente não encontradas");
 }
 
-module.exports = { env: _env.data }
+module.exports = { env: _env.data };
--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
-const cron = require('node-cron');
-const axios = require('axios');
-const { env } = require("./config/env")
-const { formatDate } = require("./utils/date")
+const cron = require("node-cron");
+const axios = require("axios");
+const { env } = require("./config/env");
 
-const chamarURLs = async() => {
-    const URLs = {
-        backend_integracao_pdv7_anotaai: env.BACKEND_INTEGRACAO_ANOTAAI_PDV7_URL
+const chamarURLs = async () => {
+  const URLs = {
+    backend_integracao_pdv7_anotaai: env.BACKEND_INTEGRACAO_ANOTAAI_PDV7_URL,
+    backend_integracao_pdv7_keeta: env.BACKEND_INTEGRACAO_KEETA_PDV7_URL,
+  };
+
+  for (const [key, url] of Object.entries(URLs)) {
+    if (url) {
+      try {
+        await axios.post(url);
+        console.log(`✅ [OK]: servico ${key} chamado com sucesso`);
+      } catch (e) {
+        console.log(
+          `🟥 [ERRO]: Ouve um erro ao chamar servico: ${key}\n` +
+            `- URL: ${url}`,
+        );
+      }
     }
+  }
+};
 
-    for (const url of Object.values(URLs)) {
-        try {
-            const response = await axios.post(url)
-            console.log("✅ [OK]: Url chamada com successo, status", response.status);
-        } catch (e) {
-            console.log("🟥 [ERRO]: Ouve um erro ao chamar a url:", url, e);
-        }
-    }
-}
-
-cron.schedule('*/30 * * * * *', () => {
-    console.log('Executando cron job:', new Date().toISOString());
-    chamarURLs()
+cron.schedule("*/30 * * * * *", () => {
+  console.log("Executando cron job:", new Date().toISOString());
+  chamarURLs();
 });
 
-console.log("🚀 Cron-iniciado")
+console.log("🚀 Cron-iniciado");
